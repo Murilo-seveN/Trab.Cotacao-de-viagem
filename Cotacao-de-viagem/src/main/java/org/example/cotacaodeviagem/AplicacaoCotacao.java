d08741ad10a3e0ac064f493295b2eb157165044c
JoseLuizDelTav <joseluoz2667@gmail.com>
2026-05-19 21:59:00 -0300

    Add files via upload

---
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/AplicacaoCotacao.java
@@ -0,0 +1,12 @@
+package org.example.cotacaodeviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotacoesApplication.class, args);
+    }
+}
