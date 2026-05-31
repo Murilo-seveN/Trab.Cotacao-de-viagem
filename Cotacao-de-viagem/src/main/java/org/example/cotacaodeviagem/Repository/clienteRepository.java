0dfa954fce640f7348463a8dc995d43ec18e2cea
Murilo_seveN <muriloneves.silva1@gmail.com>
2026-05-31 16:55:53 -0300

    Update

--- Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/Repository/clienteRepository.java
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/Repository/clienteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<clienteEntity, Long> {
+public interface clienteRepository extends JpaRepository<clienteEntity, Long> {
     Optional<Cliente> findByEmail(String email);
 }
