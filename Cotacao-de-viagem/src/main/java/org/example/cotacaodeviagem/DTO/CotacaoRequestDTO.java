017c7f92f084f5655f3d59467ec2d04de8672b80
JoseLuizDelTav <joseluoz2667@gmail.com>
2026-06-01 14:22:20 -0300

    Alteração CotacaoRequestDTO
    Refactor CotacaoRequestDTO to use LocalDateTime and Lombok.

--- Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/DTO/CotacaoRequestDTO.java
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/DTO/CotacaoRequestDTO.java
@@ -1,25 +1,12 @@
-package com.example.trabalhocotacao;
+package org.example.cotacaodeviagem.dto;
+import lombok.Data;
+import java.time.LocalDateTime;
 
+@Data
 public class CotacaoRequestDTO {
-
     private Long clienteId;
     private Long destinoId;
-    private String dataViagem;
-    private String dataRetorno;
-    private int numeroPessoas;
-
-    public Long getClienteId() { return clienteId; }
-    public void setClienteId(Long clienteId) { this.clienteId = clienteId; }
-
-    public Long getDestinoId() { return destinoId; }
-    public void setDestinoId(Long destinoId) { this.destinoId = destinoId; }
-
-    public String getDataViagem() { return dataViagem; }
-    public void setDataViagem(String dataViagem) { this.dataViagem = dataViagem; }
-
-    public String getDataRetorno() { return dataRetorno; }
-    public void setDataRetorno(String dataRetorno) { this.dataRetorno = dataRetorno; }
-
-    public int getNumeroPessoas() { return numeroPessoas; }
-    public void setNumeroPessoas(int numeroPessoas) { this.numeroPessoas = numeroPessoas; }
+    private LocalDateTime dataViagem;
+    private LocalDateTime dataRetorno;
+    private Integer numeroDePessoas;
 }
