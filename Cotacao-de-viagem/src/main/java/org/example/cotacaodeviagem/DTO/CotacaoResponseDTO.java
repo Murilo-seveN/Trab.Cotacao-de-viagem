3275848e101b9eac0911a069a0ba6bc61aba23cb
JoseLuizDelTav <joseluoz2667@gmail.com>
2026-06-01 14:23:58 -0300

    Alteração CotacaoResponseDTO

--- Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/DTO/CotacaoResponseDTO.java
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/DTO/CotacaoResponseDTO.java
@@ -1,33 +1,17 @@
-package com.example.trabalhocotacao;
+package org.example.cotacaodeviagem.dto;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+@Data
 public class CotacaoResponseDTO {
-
     private Long id;
     private Long clienteId;
     private Long destinoId;
-    private String dataViagem;
-    private String dataRetorno;
-    private int numeroPessoas;
+    private LocalDateTime dataCotacao;
+    private LocalDateTime dataViagem;
+    private LocalDateTime dataRetorno;
+    private Integer numeroDePessoas;
+    private BigDecimal valorTotal;
     private String status;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getClienteId() { return clienteId; }
-    public void setClienteId(Long clienteId) { this.clienteId = clienteId; }
-
-    public Long getDestinoId() { return destinoId; }
-    public void setDestinoId(Long destinoId) { this.destinoId = destinoId; }
-
-    public String getDataViagem() { return dataViagem; }
-    public void setDataViagem(String dataViagem) { this.dataViagem = dataViagem; }
-
-    public String getDataRetorno() { return dataRetorno; }
-    public void setDataRetorno(String dataRetorno) { this.dataRetorno = dataRetorno; }
-
-    public int getNumeroPessoas() { return numeroPessoas; }
-    public void setNumeroPessoas(int numeroPessoas) { this.numeroPessoas = numeroPessoas; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
 }
