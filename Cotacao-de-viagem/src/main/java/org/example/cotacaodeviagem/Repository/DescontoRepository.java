d048e5f307b509f6807e197ee99e337ffa3c884e
fonsecajesuspedro <fonsecajesuspedro@gmail.com>
2026-05-31 22:39:29 -0300

    Update DescontoRepository.java

--- Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/Repository/DescontoRepository.java
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/Repository/DescontoRepository.java
@@ -1,11 +1,21 @@
-package org.example.cotacaodeviagem;
+package org.example.cotacaodeviagem.repository; 
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
+ 
 
-@Repository
-public interface DescontoRepository extends JpaRepository<Desconto, Long> {
+import org.example.cotacaodeviagem.entity.DescontoEntity; 
 
-    List<Desconto> findByCotacaoId(Long cotacaoId);
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+import org.springframework.stereotype.Repository; 
+
+import java.util.List; 
+
+ 
+
+@Repository 
+
+public interface DescontoRepository extends JpaRepository<DescontoEntity, Long> { 
+
+    List<DescontoEntity> findByCotacaoId(Long cotacaoId); 
+
 }
