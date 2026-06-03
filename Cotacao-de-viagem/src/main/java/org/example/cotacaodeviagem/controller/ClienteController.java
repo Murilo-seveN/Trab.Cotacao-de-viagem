5a6d4ed92ca41c2fbf2af7b2a309fd6f94cd9853
Murilo-seveN <muriloneves.silva1@gmail.com>
2026-06-03 01:21:49 -0300

    Refactor ClienteController for improved readability

--- Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/controller/ClienteController.java
+++ Cotacao-de-viagem/src/main/java/org/example/cotacaodeviagem/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.example.cotacaodeviagem.service.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
